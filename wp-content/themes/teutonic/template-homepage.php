7a5221b30d8c1552aa247cb20dc8680244d59051
Lee Ritter <lee@leeritter.com>
2015-04-01 21:46:35 -0700

    init

---
+++ wp-content/themes/teutonic/template-homepage.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Homepage */ get_header(); ?>
+
+	<main role="main">
+
+			<h1><?php the_title(); ?></h1>
+
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+
+				<?php the_content(); ?>
+
+
+
+		<?php endwhile; ?>
+		<?php endif; ?>
+
+	</main>
+
+
+<?php get_footer(); ?>
