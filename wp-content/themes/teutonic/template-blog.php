3bd3644e7f58341f05ae2158a4c2827510dd8dc7
alextryonpdx <alextryonpdx@gmail.com>
2015-04-23 16:09:54 -0700

    more
    more responive adjustments. iframe sizing for embedded videos. all pages
    check out so far except events which needs general formatting and
    shouldnt require many adjustments for responsiveness.

--- wp-content/themes/teutonic/template-blog.php
+++ wp-content/themes/teutonic/template-blog.php
@@ -4,7 +4,7 @@
 		<section id="content-section">
 
 		<h1 class="page-title"><?php the_title(); ?></h1>
-		<div class="row">
+		<div class="blog">
 		<div class="col-5of8">
 
 			<?php
@@ -25,10 +25,6 @@
 			<img src="<?php echo get_template_directory_uri(); ?>/img/corkscrew_and_hands-web.png" alt="hands opening a bottle of wine with a corkscrew">
 
 		</div>
-	</div>
-	</section>
-
-	</main>
 
 
 <?php get_footer(); ?>
