a0c09a51f0ba03203411a52fdae7a6987f85b24a
alextryonpdx <alextryonpdx@gmail.com>
2015-06-12 09:02:37 -0700

    cellar

--- wp-content/themes/teutonic/functions.php
+++ wp-content/themes/teutonic/functions.php
@@ -541,7 +541,50 @@ function buzz_post_type() {
 // Hook into the 'init' action
 add_action( 'init', 'buzz_post_type', 0 );
 
+function cellar_wine_post_type() {
 
+    $labels = array(
+        'name'                => __( 'cellar wines', 'Post Type General Name', 'text_domain' ),
+        'singular_name'       => __( 'cellar wine', 'Post Type Singular Name', 'text_domain' ),
+        'menu_name'           => __( 'cellar wine', 'text_domain' ),
+        'name_admin_bar'      => __( 'cellar wine', 'text_domain' ),
+        'parent_item_colon'   => __( 'cellar wine', 'text_domain' ),
+        'all_items'           => __( 'All  cellar wines', 'text_domain' ),
+        'add_new_item'        => __( 'Add New cellar wine', 'text_domain' ),
+        'add_new'             => __( 'New cellar wine', 'text_domain' ),
+        'new_item'            => __( 'New cellar wine', 'text_domain' ),
+        'edit_item'           => __( 'Edit cellar wine', 'text_domain' ),
+        'update_item'         => __( 'Update cellar wine', 'text_domain' ),
+        'view_item'           => __( 'View cellar wine', 'text_domain' ),
+        'search_items'        => __( 'Search cellar wines', 'text_domain' ),
+        'not_found'           => __( 'No cellar wines found', 'text_domain' ),
+        'not_found_in_trash'  => __( 'No cellar wines found in Trash', 'text_domain' ),
+    );
+    $args = array(
+        'label'               => __( 'cellar wine', 'text_domain' ),
+        'description'         => __( 'cellar wine', 'text_domain' ),
+        'labels'              => $labels,
+        'supports'            => array( 'title', 'custom-fields' ),
+        'taxonomies'          => array(),
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'menu_position'       => 5,
+        'show_in_admin_bar'   => true,
+        'show_in_nav_menus'   => true,
+        'can_export'          => true,
+        'has_archive'         => false,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'post',
+    );
+    register_post_type( 'cellar wine', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'cellar_wine_post_type', 0 );
 /*------------------------------------*\
     Add Fields to Products
 \*------------------------------------*/
